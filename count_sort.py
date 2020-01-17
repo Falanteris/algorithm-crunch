acf402afd94d436a3f67776abeebe09dcb27e0a0
Falanteris <36617663+Falanteris@users.noreply.github.com>
2020-01-17 10:44:18 +0700

    Fixed to pass more test cases

--- count_sort.py
+++ count_sort.py
@@ -1,4 +1,3 @@
-# implementation of the counting sort algorithm in Python3
 def count_sort(array):
 	def count(num,array):
 		return len([i for i in array if(i == num)])
@@ -10,17 +9,21 @@ def count_sort(array):
 		temp  = count(i,array)
 		if(len(count_array) == 0):
 			count_array.append(temp);
+			prev = count_array[i]
 			continue;
 		count_array.append(temp+prev);
 		prev = count_array[i];
 	final_array = list(array)
+	print(count_array)
 	for i in array:
-		pointer = count_array[i]-1
-		final_array[pointer] = i;
-		count_array[i]+=1
+		print(i)
+		pointer = count_array[i]
+		print(pointer)
+		final_array[pointer-1] = i;
+		count_array[i]-=1
 	return final_array;
 
 if __name__ == '__main__':
-        inp = [3,2,1,5,8,6]
-        print("INPUT ARRAY: {}".format(inp)) 
-        print(count_sort(inp))
+	inp = [1,2,2,1]
+	print("INPUT ARRAY: {}".format(inp)) 
+	print(count_sort(inp))
