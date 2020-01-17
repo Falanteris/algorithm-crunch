20f6b65a46dd5afc7d245b88a5a1e70439464446
Falanteris <36617663+Falanteris@users.noreply.github.com>
2020-01-17 10:46:52 +0700

    Removed Debug Output

--- count_sort.py
+++ count_sort.py
@@ -14,11 +14,8 @@ def count_sort(array):
 		count_array.append(temp+prev);
 		prev = count_array[i];
 	final_array = list(array)
-	print(count_array)
 	for i in array:
-		print(i)
 		pointer = count_array[i]
-		print(pointer)
 		final_array[pointer-1] = i;
 		count_array[i]-=1
 	return final_array;
@@ -26,4 +23,4 @@ def count_sort(array):
 if __name__ == '__main__':
 	inp = [1,2,2,1]
 	print("INPUT ARRAY: {}".format(inp)) 
-	print(count_sort(inp))
+	print("OUTPUT ARRAY: {}".format(count_sort(inp)))
